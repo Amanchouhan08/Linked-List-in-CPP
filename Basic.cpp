9921cf98171a28988fec702f7f7bfb642e71c953
Aman Chouhan <ak6927887@gmail.com>
2025-10-15 20:58:07 +0530

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -1,1 +1,42 @@
 //Basic Linked List Program in C++.
+#include <iostream>
+using namespace std;
+
+// Define Node structure
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+// Function to print the linked list
+void printList(Node* n) {
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create 3 nodes
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    // Assign data and connect nodes
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+    // Print the linked list
+    cout << "Linked List elements: ";
+    printList(head);
+
+    return 0;
+}
