7c40a56076cb36200582840e405fbf55dec8707b
Aman Chouhan <ak6927887@gmail.com>
2025-10-17 20:44:31 +0530

    Create P01.cpp

---
+++ P01.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    // Create nodes
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    // Assign values and links
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    // Print list
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
