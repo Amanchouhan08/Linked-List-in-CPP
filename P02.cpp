975367dd51cb880a335b5a231b41761955255aae
Aman Chouhan <ak6927887@gmail.com>
2025-10-18 21:52:01 +0530

    Update P02.cpp

--- P02.cpp
+++ P02.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head = NULL;
+    Node* temp = NULL;
+    Node* newNode = NULL;
+    int n, value;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter value for node " << i + 1 << ": ";
+        cin >> value;
+
+        // Create a new node
+        newNode = new Node();
+        newNode->data = value;
+        newNode->next = NULL;
+
+        // If it's the first node, make it head
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Print the linked list
+    cout << "\nLinked List elements: ";
+    temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
 
