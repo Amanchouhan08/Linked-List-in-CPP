3816172f1bb3f544bb738d2135669be0ffe452b5
Aman Chouhan <ak6927887@gmail.com>
2025-10-20 22:10:15 +0530

    Update p04.cpp*

--- p04.cpp
+++ p04.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Function to insert at beginning
+void insertAtBeginning(Node*& head, int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+
+// Function to print the list
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = NULL;
+
+    insertAtBeginning(head, 30);
+    insertAtBeginning(head, 20);
+    insertAtBeginning(head, 10);
+
+    cout << "Linked List after inserting at beginning: ";
+    printList(head);
+
+    return 0;
+}
+
+/
+
 
